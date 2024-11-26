faa3b5b3bf4f6c792ea0d2ca392ab48f45ff7654
Maxim <Safronovmaxiim@mail.ru>
2024-11-26 13:33:24 +0300

    Added function generateName, passes data to Figma, function to load Fonts for Figma ( async loadFonts)

--- code.js
+++ code.js
@@ -1,8 +1,18 @@
+async function loadFonts(fontNames) {
+    if (Array.isArray(fontNames)) {
+        for (const fontName of fontNames) {
+            await figma.loadFontAsync(fontName);
+        }
+    } else {
+        await figma.loadFontAsync(fontNames);
+    }
+}
+
 figma.showUI(__html__, { width: 400, height: 250 });
 
 let currentHeight = 250;
 
-figma.ui.onmessage = (msg) => {
+figma.ui.onmessage = async (msg) => {
     if (msg.type === 'block') {
         if (currentHeight === 250) {
             figma.ui.resize(400, 600);
@@ -10,12 +20,22 @@ figma.ui.onmessage = (msg) => {
         }
     }
 
-    if (msg.type === 'generateName') {
-        const { settings } = msg;
-        const name = generateName(settings);  // Генерация имени
-        figma.ui.postMessage({
-            type: 'generatedName', // Отправка результата обратно в UI
-            name: name
-        });
+    if (msg.type === 'updateText') {
+        const selectedNodes = figma.currentPage.selection;
+
+        if (selectedNodes.length > 0) {
+            for (const node of selectedNodes) {
+                if (node.type === 'TEXT') {
+                    try {
+                        await loadFonts(node.fontName);
+                        node.characters = msg.content;
+                    } catch (error) {
+                        figma.notify(`Ошибка загрузки шрифта: ${error.message}`);
+                    }
+                }
+            }
+        } else {
+            figma.notify("Выберите текстовый объект, чтобы обновить имя.");
+        }
     }
 };
