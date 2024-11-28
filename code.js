60525450a5511b50c4cff4614d4d2a5178fdb188
Maxim <Safronovmaxiim@mail.ru>
2024-11-28 13:32:17 +0300

    logic of obtaining unique names when selecting several fields

--- code.js
+++ code.js
@@ -25,13 +25,35 @@ figma.ui.onmessage = async (msg) => {
         const selectedNodes = figma.currentPage.selection;
 
         if (selectedNodes.length > 0) {
+            const generatedTexts = new Set();
+
             for (const node of selectedNodes) {
                 if (node.type === 'TEXT') {
                     try {
-                        await loadFonts(node.fontName);
-                        node.characters = msg.content;
+                        if (node.fontName && typeof node.fontName === 'object') {
+                            await figma.loadFontAsync(node.fontName);
+                        } else {
+                            console.error('fontName не найден или имеет неправильный формат', node.fontName);
+                            figma.notify('Ошибка: Шрифт для узла не найден.');
+                            continue;
+                        }
+
+                        let uniqueText;
+                        do {
+                            uniqueText = generateName({
+                                lang: msg.lang || 'en',
+                                gender: msg.gender || 'male',
+                                length: msg.length || 'random',
+                                formatName: msg.formatName || 'full',
+                                transform: msg.transform || 'none',
+                            });
+                        } while (generatedTexts.has(uniqueText));
+
+                        generatedTexts.add(uniqueText);
+                        node.characters = uniqueText;
                     } catch (error) {
-                        figma.notify(`Ошибка загрузки шрифта: ${error.message}`);
+                        console.error('Ошибка при обработке текста:', error);
+                        figma.notify('Ошибка обработки текста или загрузки шрифта.');
                     }
                 }
             }
